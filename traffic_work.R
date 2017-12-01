d370468c735251388e66d2d0ef7996dda11289f3
ghidalgo93 <ghidalgo93@gmail.com>
2017-12-01 13:24:47 -0700

    nothing new I don't think

---
+++ traffic_work.R
@@ -0,0 +1,10 @@
+#Gerardo Hidalgo-Cuellar
+#November 29, 2017
+#Looking at some traffic data
+
+#sets the working directory 
+setwd("~/Dropbox/traffic_data")
+
+#importing data
+train <- read.csv("~/Dropbox/traffic_data/Traffic_Violations.csv")
+
